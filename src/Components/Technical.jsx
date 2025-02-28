44944edbd222c6785a7f9b1becd3bba1daf03171
jhapriyanshu19 <prnshjh@gmail.com>
2025-02-28 11:47:31 +0530

    update

--- src/Components/Technical.jsx
+++ src/Components/Technical.jsx
@@ -7,7 +7,7 @@ import {FaNodeJs} from 'react-icons/fa';
 const Technical = () => {
   return (
     <div className='border-b border-neutral-800 pb-24 '>
-     <h1 className='my-20 text-center text-4xl'>Technologies</h1>
+     <h2 className='my-20 text-center text-4xl'>Technologies</h2>
      <div className='flex flex-wrap items-center justify-center gap-4'>
         <div className='rounded-2xl border-4 border-neutral-800 p-4'>
             <RiReactjsLine className='text-7xl text-cyan-400'/>
